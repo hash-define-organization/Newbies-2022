0dabdbfa58d7b6a9c4d9f7c67e5fba96e7dde8c1
Mansiuniyal <mansiuniyal2703@gmail.com>
2022-10-27 10:47:56 +0530

    Digital_Diary

---
+++ web/Digital_Diary/connection.php
@@ -0,0 +1,9 @@
+<?php 
+ $link = mysqli_connect("localhost","uname","","dbname");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+		?>
